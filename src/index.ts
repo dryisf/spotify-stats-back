43bdcf0c6554ba05428af706f589c5bee182e3c5
dryisf <daryl.issoufi@gmail.com>
2024-10-16 09:11:28 +0200

    feat: add access control allow origin header

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,10 @@ app.use(express.json());
 app.use(function (req: Request, res: Response, next) {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://spotify-stats-front-lqac9bwdr-dryisfs-projects.vercel.app"
+  );
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
